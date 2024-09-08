27524ad08339914c28adf701d2db1bda1891899b
emil-oestergaard <emailoestergaard03@gmail.com>
2024-09-08 17:40:04 +0200

    Implemented saving of password to file and improved memory allocation

--- password-manager.c
+++ password-manager.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
-void generate_password(int length, int use_special, char *password);
+void generate_password(int length, const char *characters, char *password);
+void save_to_file(FILE *file, const char *title, const char *password);
 
 const char *ALPHA_NUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 const char *SPECIAL_CHARS = "!@#$%^&*()-_=+[]{}|;:',.<>?";
@@ -12,6 +14,8 @@ int main(int argc, char *argv[])
 {
     int length = 12;
     int use_special = 0;
+    const char *filename = "passwords.txt";
+    char *title = NULL;
 
     for (int i = 1; i < argc; i++)
     {
@@ -23,28 +27,39 @@ int main(int argc, char *argv[])
         {
             use_special = 1;
         }
+        else if (strcmp(argv[i], "-t") == 0 && i + 1 < argc)
+        {
+            const char *input_title = argv[++i];
+            title = malloc(strlen(input_title) + 1);
+            if (title == NULL)
+            {
+                printf("Failed to allocate memory for title\n");
+                return 1;
+            }
+
+            for (size_t j = 0; j < strlen(input_title); j++)
+            {
+                title[j] = tolower((unsigned char)input_title[j]);
+            }
+            title[strlen(input_title)] = '\0';
+        }
+    }
+
+    if (title == NULL)
+    {
+        title = "unnamed";
     }
 
     char *password = malloc(length + 1);
     if (password == NULL)
     {
-        printf("Failed to allocate memory\n");
+        printf("Failed to allocate memory for password\n");
+        free(title);
         return 1;
     }
 
-    generate_password(length, use_special, password);
-    password[length] = '\0';
-
-    printf("Generated password: %s\n", password);
-
-    free(password);
-    return 0;
-}
-
-void generate_password(int length, int use_special, char *password)
-{
     const char *characters = ALPHA_NUMERIC;
-    char *combined_characters;
+    char *combined_characters = NULL;
 
     if (use_special)
     {
@@ -52,24 +67,56 @@ void generate_password(int length, int use_special, char *password)
         if (combined_characters == NULL)
         {
             printf("Failed to allocate memory for combined characters\n");
-            exit(1);
+            free(title);
+            free(password);
+            return 1;
         }
 
         strcpy(combined_characters, ALPHA_NUMERIC);
         strcat(combined_characters, SPECIAL_CHARS);
-
         characters = combined_characters;
     }
 
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
 
-    for (int i = 0; i < length; i++)
-    {
-        password[i] = characters[rand() % strlen(characters)];
-    }
+    generate_password(length, characters, password);
+    password[length] = '\0';
+
+    printf("Generated password: %s\n", password);
 
     if (use_special)
     {
         free(combined_characters);
     }
+
+    FILE *file = fopen(filename, "a");
+    if (file)
+    {
+        save_to_file(file, title, password);
+        fclose(file);
+    }
+    else
+    {
+        printf("Failed to open file: %s\n", filename);
+        free(title);
+        free(password);
+        return 1;
+    }
+
+    free(title);
+    free(password);
+    return 0;
+}
+
+void generate_password(int length, const char *characters, char *password)
+{
+    for (int i = 0; i < length; i++)
+    {
+        password[i] = characters[rand() % strlen(characters)];
+    }
+}
+
+void save_to_file(FILE *file, const char *title, const char *password)
+{
+    fprintf(file, "%s: %s\n\n", title, password);
 }
